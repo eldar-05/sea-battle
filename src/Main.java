ebd4f565b35fd5840b796e6d16ad3496a2239689
eldar-05 <eldarzh123@gmail.com>
2023-11-24 12:11:03 +0600

    Final commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 import java.util.Scanner;
-public class battleShipGameProject
+public class Main
 {
     public static void main(String[] args){
         Random random = new Random();
